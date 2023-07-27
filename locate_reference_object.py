314c6791fa251af63deca07ac7989aceda0521e4
Maidul <u1703016@student.cuet.ac.bd>
2023-07-27 16:40:48 +0600

    [chore] provide more descriptive output msg if corners are not found

--- locate_reference_object.py
+++ locate_reference_object.py
@@ -16,15 +16,15 @@ def contour_perimeter(cnt):
     return cv.arcLength(cnt, closed=True)
 
 
-def find_ref_object_corners(preprocessed_img):
+def find_corners(preprocessed_img):
     """
     The input should be a processed binary 8 bit image.
-    Returns the 4 corner points of the A4 paper.
+    Returns the 4 corner points of the A4 paper in np.float32 data format.
     In counter-clockwise direction starting from top right corner,
     i.e, top right, top left, bottom left and bottom right
     """
 
-    cnts, hier = cv.findContours(
+    cnts, _ = cv.findContours(
         preprocessed_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
     )
     # find the contour with the largest perimeter
@@ -32,7 +32,17 @@ def find_ref_object_corners(preprocessed_img):
     # find the corners
     # output will be of shape (4, 1, 2).
     # keep it like that if you are to draw bounding box with cv.polylines
-    corners = cv.approxPolyDP(
-        ref_obj_cnt, 0.01 * contour_perimeter(ref_obj_cnt), closed=True
+    corners = cv.convexHull(
+        cv.approxPolyDP(ref_obj_cnt, 0.01 * contour_perimeter(ref_obj_cnt), closed=True)
     )
-    return np.squeeze(corners)
+    if corners.shape[0] != 4:
+        print("Couldn't detect reference object (A4 Paper).\n")
+        print("You can try the following to make your image more recognizable : \n")
+        print(
+            """\t 1. Capture the image in a cleaner background. \n\t 
+            2. Try to fit the whole A4 paper inside the frame. \n\t 
+            3. Or, try again with different image.\n"""
+        )
+        return None
+    else:
+        return np.squeeze(corners).astype(np.float32)
