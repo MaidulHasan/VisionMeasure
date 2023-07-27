f8788596ffc1380f192457758352f5178dafb8dd
Maidul <u1703016@student.cuet.ac.bd>
2023-07-27 16:39:05 +0600

    [perf] Adapts to more generalized image backgrounds, uses cv.threshold() with thresh_val = 130 instead of cv.adaptiveThreshold()

--- preprocessing.py
+++ preprocessing.py
@@ -19,27 +19,16 @@ def preprocess(img):
     grayscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     grayscale_img_blurred = cv.GaussianBlur(grayscale_img, (5, 5), 0, 0)
 
-    # C = 0 will produce an image that outlines the edges as white
-    # Gaussian Kernel of size 3 works best (produces less white noise)
-    # So first erosion to minimize white noise (more than 1 iteration will eliminate the edges)
-    # Then dilation to widen the edges (2 or 3 iterations are enough)
-    thresholded_img = cv.adaptiveThreshold(
-        grayscale_img_blurred,
-        255,
-        cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv.THRESH_BINARY,
-        3,
-        0,
-    )
+    # By experimenting we've found, threshold = 130 works best for a wide range of image backgrounds
+    _, thresholded_img = cv.threshold(grayscale_img_blurred, 130, 255, cv.THRESH_BINARY)
+
+    # Erosion to detach any contour that remains loosely connected to the A4 paper contour
     morph_operation_kernel = np.ones(
         (3, 3), dtype=np.uint8
     )  # same as MORPH_RECT of size 3
     thresholded_img_eroded = cv.erode(
-        thresholded_img, morph_operation_kernel, iterations=1
-    )
-    eroded_img_dilated = cv.dilate(
-        thresholded_img_eroded, morph_operation_kernel, iterations=3
+        thresholded_img, morph_operation_kernel, iterations=2
     )
 
-    preprocessed_img = eroded_img_dilated
+    preprocessed_img = thresholded_img_eroded
     return preprocessed_img
