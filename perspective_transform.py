0e94e42a6160db3d6b240a0b53e0990bc2b97bef
Maidul <u1703016@student.cuet.ac.bd>
2023-07-29 15:09:16 +0600

    [perf] moved the scaling to matplotlib_imshow function for better calculation accuracy

--- perspective_transform.py
+++ perspective_transform.py
@@ -10,22 +10,19 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------------------------------------------
 ###                                      Perspective Transform                              ###
 # -----------------------------------------------------------------------------------------------
-def perspective_transform(source_img, actual_points, scale=4, pad=30):
+def perspective_transform(source_img, actual_points, pad=5):
     """
     source_img: image to transform (you shold pass in the original RGB image as it can be manipulated later).
 
     actual_points: 4 points on the source image (3 of which mustn't be colinear). Must be in the
     (top left, top right, bottom left, bottom right) order.
 
-    scale: the image is by default transformed to an image of, w = (210 * scale) and h = (297 * scale).
-    The to be points for perspective transformation is calculated from these w and h values. default is 4.
-
     pad: to apply padding to the perspective transformed image (to skip padding set, pad=0)
 
-    Returns: a tuple of (scale, Padded perspective transformed image).
+    Returns: perspective transformed image of shape (210, 297) [note: this shape is without padding].
     """
-    w = scale * 210
-    h = scale * 297
+    w = 210
+    h = 297
 
     to_be_points = np.array([[0, 0], [w, 0], [0, h], [w, h]], dtype=np.float32)
 
@@ -42,4 +39,4 @@ def perspective_transform(source_img, actual_points, scale=4, pad=30):
         pad : h - pad, pad : w - pad
     ]
 
-    return (scale, padded_perspective_transformed_image)
+    return padded_perspective_transformed_image
