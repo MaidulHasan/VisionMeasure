99591b83415b3fee83fe3ed73db9f1eec01f0fd3
Maidul <u1703016@student.cuet.ac.bd>
2023-07-29 16:07:26 +0600

    draw a rotated bounding box around the detected object and show the dimensions in the top left corner of the image.

---
+++ visualize_detections.py
@@ -0,0 +1,47 @@
+# -----------------------------------------------------------------------------------------------
+###                                         Imports                                         ###
+# -----------------------------------------------------------------------------------------------
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+from calculate_dimensions import calculate_dimensions
+from matplotlib_imshow import matplotlib_imshow
+
+
+# -----------------------------------------------------------------------------------------------
+###                                      Visualize Detections                                ###
+# -----------------------------------------------------------------------------------------------
+def visualize_detections(src, pts):
+    """
+    src: image on which to draw the rotated rectangle and display the calculated width and height.
+         should be BGR type image.
+    pts: points array that outlines the object (to call the calculate_dimensions() function)
+
+    Returns the modified image (BGR type).
+    """
+    src = src.copy()
+    box2d_obj, (w, h) = calculate_dimensions(pts)
+    corner_points = cv.boxPoints(box2d_obj).astype(np.int32)
+    cv.polylines(src, [corner_points], isClosed=True, color=(0, 255, 0), thickness=2)
+    cv.putText(
+        src,
+        f"Width: {round(w/10, 1)} cm",
+        (10, 25),
+        cv.FONT_HERSHEY_SCRIPT_SIMPLEX,
+        0.5,
+        (255, 0, 255),
+        1,
+    )
+    cv.putText(
+        src,
+        f"Height: {round(h/10, 1)} cm",
+        (10, 50),
+        cv.FONT_HERSHEY_SCRIPT_SIMPLEX,
+        0.5,
+        (255, 0, 255),
+        1,
+    )
+
+    return src
